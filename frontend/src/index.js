2a1e3f30d9d236602fd0daeed87865f1f5487c61
fariasrobson.rf@gmail.com <fariasrobson.rf@gmail.com>
2018-06-09 10:04:13 -0700

    newStore dispatch fetchStadiums and fetchTeams

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import FlagIconFactory from 'react-flag-icon-css'
+
 import 'bootswatch/dist/materia/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import store from './redux/store';
 import { fetchToken } from './redux/actions/auth';
+import { matchesByDay } from './redux/actions/matches';
+import { fetchTeams } from './redux/actions/matches';
+import { fetchStadiums } from './redux/actions/matches';
 
 const newStore = store();
 
 newStore.dispatch(fetchToken())
+newStore.dispatch(matchesByDay())
+newStore.dispatch(fetchTeams())
+newStore.dispatch(fetchStadiums())
 
 ReactDOM.render(
   <Provider store={newStore}>
